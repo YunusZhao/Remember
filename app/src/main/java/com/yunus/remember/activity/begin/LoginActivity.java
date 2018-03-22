341657db786b176d5b84d46b6b4257da6663cb7c
YunusZhao <yunuszhao@163.com>
2018-03-22 20:02:43 +0800

    login界面初稿

--- app/src/main/java/com/yunus/remember/activity/begin/LoginActivity.java
+++ app/src/main/java/com/yunus/remember/activity/begin/LoginActivity.java
@@ -71,6 +71,7 @@ public class LoginActivity extends BaseActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+
             }
         });
 
