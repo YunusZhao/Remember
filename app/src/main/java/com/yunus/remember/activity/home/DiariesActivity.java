5f7f555c9d27fd09c1cde6b164c86fea6ee1cdbd
YunusZhao <yunuszhao@163.com>
2018-04-02 10:03:38 +0800

    完成打卡日记和全部日记页面

---
+++ app/src/main/java/com/yunus/remember/activity/home/DiariesActivity.java
@@ -0,0 +1,16 @@
+package com.yunus.remember.activity.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yunus.activity.BaseActivity;
+import com.yunus.remember.R;
+
+public class DiariesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diaries);
+    }
+}
---
+++ app/src/main/java/com/yunus/remember/activity/home/DiaryActivity.java
@@ -0,0 +1,15 @@
+package com.yunus.remember.activity.home;
+
+import android.os.Bundle;
+
+import com.example.yunus.activity.BaseActivity;
+import com.yunus.remember.R;
+
+public class DiaryActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
