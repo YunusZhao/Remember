ee1b50e5b0654be9bd8fe160d226ca624924eb4a
YunusZhao <yunuszhao@163.com>
2018-04-03 15:36:43 +0800

    全部界面基本完成

---
+++ app/src/main/java/com/yunus/remember/activity/chief/NoticeContentActivity.java
@@ -0,0 +1,16 @@
+package com.yunus.remember.activity.chief;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yunus.activity.BaseActivity;
+import com.yunus.remember.R;
+
+public class NoticeContentActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_content);
+    }
+}
---
+++ app/src/main/java/com/yunus/remember/activity/mine/FriendNewActivity.java
@@ -0,0 +1,16 @@
+package com.yunus.remember.activity.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yunus.activity.BaseActivity;
+import com.yunus.remember.R;
+
+public class FriendNewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_new);
+    }
+}
