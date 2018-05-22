7f26ca4bf30218ada44a9abd64240ec806d4b22e
YunusZhao <yunuszhao@163.com>
2018-05-22 11:47:46 +0800

    完善单词列表

--- app/src/main/java/com/yunus/remember/activity/mine/MineWordsActivity.java
+++ app/src/main/java/com/yunus/remember/activity/mine/MineWordsActivity.java
@@ -1,6 +1,8 @@
 package com.yunus.remember.activity.mine;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -27,11 +29,22 @@ public class MineWordsActivity extends BaseActivity implements RadioGroup.OnChec
     private ListView wordListView;
     private List<Word> wordList = new ArrayList<>();
     private List<TodayWord> todayWordList;
+    WordAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mine_words);
+
+        Toolbar toolbar = findViewById(R.id.mine_words_toolbar);
+        toolbar.setTitle("单词列表");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         today = findViewById(R.id.today_word);
         newWord = findViewById(R.id.new_word);
@@ -41,7 +54,7 @@ public class MineWordsActivity extends BaseActivity implements RadioGroup.OnChec
 
         bindViews();
 
-        WordAdapter adapter = new WordAdapter(this, R.layout.item_word, wordList);
+        adapter = new WordAdapter(this, R.layout.item_word, wordList);
         wordListView.setAdapter(adapter);
     }
 
@@ -50,19 +63,23 @@ public class MineWordsActivity extends BaseActivity implements RadioGroup.OnChec
         switch (i) {
             case R.id.new_word:
                 wordList = DataSupport.where("level = 5").find(Word.class);
-                wordListView.deferNotifyDataSetChanged();
+                adapter = new WordAdapter(this, R.layout.item_word, wordList);
+                wordListView.setAdapter(adapter);
                 break;
             case R.id.study_word:
                 wordList = DataSupport.where("level < 5 and level > 0").find(Word.class);
-                wordListView.deferNotifyDataSetChanged();
+                adapter = new WordAdapter(this, R.layout.item_word, wordList);
+                wordListView.setAdapter(adapter);
                 break;
             case R.id.had_word:
                 wordList = DataSupport.where("level = 0").find(Word.class);
-                wordListView.deferNotifyDataSetChanged();
+                adapter = new WordAdapter(this, R.layout.item_word, wordList);
+                wordListView.setAdapter(adapter);
                 break;
             case R.id.simple_word:
                 wordList = DataSupport.where("level = -1").find(Word.class);
-                wordListView.deferNotifyDataSetChanged();
+                adapter = new WordAdapter(this, R.layout.item_word, wordList);
+                wordListView.setAdapter(adapter);
                 break;
             case R.id.today_word:
             default:
@@ -71,7 +88,8 @@ public class MineWordsActivity extends BaseActivity implements RadioGroup.OnChec
                     wordList.add(new Word(todayWord.getSpell(), todayWord.getMean(), todayWord
                             .getPhonogram()));
                 }
-                wordListView.deferNotifyDataSetChanged();
+                adapter = new WordAdapter(this, R.layout.item_word, wordList);
+                wordListView.setAdapter(adapter);
                 break;
         }
     }
